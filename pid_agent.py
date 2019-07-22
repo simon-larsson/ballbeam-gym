7f25694d46494009cb6dc2be76580eb0fad2ddba
Simon Larsson <simon.larsson@seavus.com>
2019-07-22 15:24:16 +0200

    simplified PID

--- pid_agent.py
+++ pid_agent.py
@@ -10,40 +10,15 @@ env = BallBeamSetpointEnv(time_step=TIME_STEP,
                           max_angle=MAX_ANGLE)
 
 Kp = 2.0
-Ki = 0.0
 Kd = 1.0
 
-class PID():
-
-    def __init__(self, setpoint, Kp=2.0, Ki = 0.0, Kd=1.0):
-        self.setpoint = setpoint
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd = Kd
-        self.err = 0.0
-        self.I_err = 0.0
-
-    def update(self, value, dt):
-        error = value - self.setpoint
-        delta_error = error - self.err
-        self.I_err += error*dt
-        self.err = error
-
-        return self.Kp*error + self.Ki*self.I_err + (delta_error*self.Kd) / dt
-
-    def reset(self):
-        self.err = 0.0
-        self.I_err = 0.0
-
-pid = PID(SETPOINT, Kp, Ki, Kd)
-
 for i in range(1000):
-    theta = pid.update(env.bb.x, TIME_STEP)
+    # PID-calculation (I-part skipped since it only hurts here)  
+    theta = Kp*(env.bb.x - SETPOINT) + Kd*(env.bb.v)
     obs, reward, done, info = env.step(theta)
     env.render()
 
     if done:
-        pid.reset()
         env.reset()
 
 env.close()
