f5143da334668b5400610f84c03ee4ccf2b9a48c
Simon Larsson <simon.larsson@seavus.com>
2019-07-22 21:35:36 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='ballbeam-gym',
+    version='0.0.1',
+    description='Ball & beam environments for OpenAI gym',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    author='Simon Larsson',
+    author_email='simonlarsson0@gmail.com',
+    url='https://github.com/simon-larsson/ballbeam-gym',
+    license='MIT',
+    install_requires=['gym', 'numpy', 'matplotlib'],
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Topic :: Scientific/Engineering']
+)
